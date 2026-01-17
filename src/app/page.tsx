af909f6d104e45fd11b06eb8c2abba257276c1d1
John Trinidad <trinidj03@protonmail.com>
2026-01-17 09:14:41 -0500

    feat(combat_roles): correct spelling of Off-Tune Buildup Efficiency and update related assets
    feat(resonators): update resonator attributes and add new images for 5-star resonators
    
    feat(page): implement Current Banners section with resonator links and images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 import Image from "next/image";
+import Link from "next/link";
+
+import { Card, CardContent } from "@/components/ui/card";
+
+const CURRENT_BANNERS = [
+  { name: "Lynae", rarity: 5 },
+  { name: "Cartethyia", rarity: 5 },
+  { name: "Ciaccona", rarity: 5 }
+] as const;
+
+function getResonatorSheetImage(name: string, rarity: 4 | 5) {
+  return `/assets/resonators/${rarity}_star/${name}/sheet.png`;
+}
+
+function getWeaponSheetImage(name: string) {
+  
+}
+
+function getResonatorUrl(name: string) {
+  return `/pages/resonators/${name.toLowerCase()}`;
+}
 
 export default function Home() {
   return (
-    <div className="min-h-[60vh] bg-background">
-      <section className="flex justify-center text-center py-8 md:py-12 lg:py-16">
+    <div className="min-h-[60vh] bg-background flex flex-col gap-40">
+      <section className="flex justify-center text-center">
         <div className="flex flex-col items-center justify-center gap-6 md:gap-8 lg:gap-10 px-4">
           <Image
             src="/assets/site_icon.png"
@@ -20,6 +41,34 @@ export default function Home() {
               The Tethys System is an unofficial Wuthering Waves database and tools site,
               offering clear stats, materials, and builds.
             </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="flex justify-cente">
+        <div>
+          <div className="flex flex-col md:gap-2 max-w-4xl">
+            <h2 className="text-lg font-bold sm:text-2xl md:text-3xl lg:text-3xl">Current Banners</h2>
+            <p className="text-sm text-muted-foreground sm:text-base md:text-lg">See what's available in the current Convene banners</p>
+          </div>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+            {CURRENT_BANNERS.map((banner) => (
+              <Link key={banner.name} href={getResonatorUrl(banner.name)}>
+                <Card className="overflow-hidden gap-0 p-0 max-w-xl border-none shadow-none transition-transform will-change-transform duration-500 hover:scale-105">
+                  <CardContent className="px-0">
+                    <Image
+                      src={getResonatorSheetImage(banner.name, banner.rarity)}
+                      alt={`${banner.name} Banner`}
+                      width={1000}
+                      height={563}
+                      sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 400px"
+                      className="w-full h-auto object-contain"
+                    />
+                  </CardContent>
+                </Card>
+              </Link>
+            ))}
           </div>
         </div>
       </section>
